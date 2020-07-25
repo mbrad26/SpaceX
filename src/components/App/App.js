a4a7ce78f698b51a8a32fcffa7dc7b50a4a68627
mbrad26 <mbrad26@gmail.com>
2020-07-25 16:16:55 +0100

    fetched data and displays img

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import {
-  Card,
-  Text,
-  Loader,
-  Button,
-} from '@mrshmllw/smores-react';
+import { Text, Loader, Button } from '@mrshmllw/smores-react';
+import axios from 'axios';
 
-const App = () => (
-  <>
-    <Card >
+const API_ENDPOINT = 'https://api.spacexdata.com/v3/';
+const getUrl = query => `${API_ENDPOINT}${query}`;
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false)
+
+  const handleFetchData = async event => {
+    try {
+      const result = await axios.get(getUrl(event.target.value));
+      setData(result.data);
+    } catch {
+      setError(true);
+    }
+  }
+  console.log('Comp: A');
+  useEffect(() => {
+    handleFetchData()
+  }, []);
+
+  return (
+    <>
       <Text tag='h1' typo='header'>Hello World</Text>
-      <Loader height='20' /><br/>
-      <Button color="pink">Click Me!</Button>
-    </Card>
-  </>
-);
+      <Loader height='20' />
+
+      <br/>
+
+      <button value='rockets' onClick={handleFetchData}>Rockets</button>
+      <button value='dragons' onClick={handleFetchData}>Dragons</button>
+
+      {error && <h3>Something went terribly wrong ...</h3>}
+      {data &&
+        <ul>{data.map(item =>
+          <li key={item.id}>
+            {item.flickr_images.map((url) => <img src={url} key={url} width='200'/>)}
+          </li>)}
+        </ul>
+      }
+    </>
+  )
+};
 
 export default App;
