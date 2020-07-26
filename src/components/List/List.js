eeecc3c7add53ad3f8e6d9359d2176236ad6e070
mbrad26 <mbrad26@gmail.com>
2020-07-26 14:02:23 +0100

    extracted Item component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,12 +2,22 @@ import React from 'react';
 
 const List = ({ data }) => {
   return (
-    <ul>{data.map(item =>
-      <li key={item.id}>
-        {item.flickr_images.map((url) => <img src={url} key={url} width='200'/>)}
-      </li>)}
+    <ul>
+      {data.map(
+        item => <Item key={item.id} item={item} />
+      )}
     </ul>
   )
-}
+};
+
+const Item = ({ item }) => {
+  return (
+    <li>
+      {item.flickr_images.map(
+        url => <img src={url} key={url} width='200' />
+      )}
+    </li>
+  )
+};
 
 export default List;
