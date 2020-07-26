53e2ea954c8d7f5e3333e62f4823240b7c5da050
mbrad26 <mbrad26@gmail.com>
2020-07-26 11:35:31 +0100

    added useReducer to manage state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import './App.css';
 import { Text, Loader, Button } from '@mrshmllw/smores-react';
 import axios from 'axios';
@@ -6,21 +6,55 @@ import axios from 'axios';
 const API_ENDPOINT = 'https://api.spacexdata.com/v3/';
 const getUrl = query => `${API_ENDPOINT}${query}`;
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'DATA_LOADING':
+      return {
+        ...state,
+        loading: true,
+        error: false,
+      };
+    case 'DATA_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        error: false,
+        data: action.payload,
+      };
+    case 'DATA_ERROR':
+      return {
+        ...state,
+        loading: false,
+        error: true,
+      }
+    default:
+      throw new Error();
+  }
+};
+
 const App = () => {
-  const [data, setData] = useState([]);
-  const [error, setError] = useState(false)
+  const initialState = { data: [], loading: false, error: false };
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const handleFetchData = async event => {
+    dispatch({ type: 'DATA_LOADING' });
+
     try {
       const result = await axios.get(getUrl(event.target.value));
-      setData(result.data);
+      dispatch({
+        type: 'DATA_SUCCESS',
+        payload: result.data
+      });
+      console.log(state.data);
     } catch {
-      setError(true);
+      dispatch({ type: 'DATA_ERROR' });
     }
   }
+
   console.log('Comp: A');
+
   useEffect(() => {
-    handleFetchData()
+    handleFetchData();
   }, []);
 
   return (
@@ -33,14 +67,18 @@ const App = () => {
       <button value='rockets' onClick={handleFetchData}>Rockets</button>
       <button value='dragons' onClick={handleFetchData}>Dragons</button>
 
-      {error && <h3>Something went terribly wrong ...</h3>}
-      {data &&
-        <ul>{data.map(item =>
-          <li key={item.id}>
-            {item.flickr_images.map((url) => <img src={url} key={url} width='200'/>)}
-          </li>)}
-        </ul>
-      }
+      {state.error && <h3>Something went terribly wrong ...</h3>}
+      
+      {state.loading
+        ? (<p>Loading ...</p>)
+        : (
+            <ul>{state.data.map(item =>
+              <li key={item.id}>
+                {item.flickr_images.map((url) => <img src={url} key={url} width='200'/>)}
+              </li>)}
+            </ul>
+          )
+        }
     </>
   )
 };
