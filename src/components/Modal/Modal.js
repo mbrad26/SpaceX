cde1591ada57b43abe37f638b8dbf08ef63b3730
mbrad26 <mbrad26@gmail.com>
2020-07-30 09:56:23 +0100

    fixed dragons modal to show aditional details

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -55,28 +55,49 @@ const ModalComponent = () => {
           <p><span>Diameter in meters: </span>{activeItem.diameter.meters}</p>
           <p><span>Diameter in feet: </span>{activeItem.diameter.feet}</p>
 
-          <p><span>Engines layout: </span>{activeItem.engines.layout}</p>
-          <p><span>Engines number: </span>{activeItem.engines.number}</p>
-          <p><span>Engines propellant 1: </span>{activeItem.engines.propellant_1}</p>
-          <p><span>Engines propellant 2: </span>{activeItem.engines.propellant_2}</p>
-          <p><span>Thrust at sea level in kN: </span>{activeItem.engines.thrust_sea_level.kN}</p>
-          <p><span>Thrust at sea level in lbf: </span>{activeItem.engines.thrust_sea_level.lbf}</p>
+          {activeItem.engines &&
+            <>
+              <p><span>Engines layout: </span>{activeItem.engines.layout}</p>
+              <p><span>Engines number: </span>{activeItem.engines.number}</p>
+              <p><span>Engines propellant 1: </span>{activeItem.engines.propellant_1}</p>
+              <p><span>Engines propellant 2: </span>{activeItem.engines.propellant_2}</p>
+              <p><span>Thrust at sea level in kN: </span>{activeItem.engines.thrust_sea_level.kN}</p>
+              <p><span>Thrust at sea level in lbf: </span>{activeItem.engines.thrust_sea_level.lbf}</p>
+            </>
+          }
+
 
           <p><span>First flight: </span>{activeItem.first_flight}</p>
 
-          <p><span>Height in feet: </span>{activeItem.height.feet}</p>
-          <p><span>Height in meters: </span>{activeItem.height.meters}</p>
+          {activeItem.height &&
+            <>
+              <p><span>Height in feet: </span>{activeItem.height.feet}</p>
+              <p><span>Height in meters: </span>{activeItem.height.meters}</p>
+            </>
+          }
 
-          <p><span>Landing legs number: </span>{activeItem.landing_legs.number}</p>
-          <p><span>Landing legs material: </span>{activeItem.landing_legs.material}</p>
+          {activeItem.landing_legs &&
+            <>
+              <p><span>Landing legs number: </span>{activeItem.landing_legs.number}</p>
+              <p><span>Landing legs material: </span>{activeItem.landing_legs.material}</p>
+            </>
+          }
 
-          <p><span>Mass in kg: </span>{activeItem.mass.kg}</p>
-          <p><span>Mass lb: </span>{activeItem.mass.lb}</p>
+          {activeItem.mass &&
+            <>
+              <p><span>Mass in kg: </span>{activeItem.mass.kg}</p>
+              <p><span>Mass lb: </span>{activeItem.mass.lb}</p>
+            </>
+          }
 
-          <p><span>Rocket type: </span>{activeItem.rocket_type}</p>
+          {activeItem.rocket_type && <p><span>Rocket type: </span>{activeItem.rocket_type}</p>}
 
-          <p><span>Second stage engines: </span>{activeItem.second_stage.engines}</p>
-          <p><span>Second stage fuel amount: </span>{activeItem.second_stage.fuel_amount_tons}</p>
+          {activeItem.second_stage &&
+            <>
+              <p><span>Second stage engines: </span>{activeItem.second_stage.engines}</p>
+              <p><span>Second stage fuel amount: </span>{activeItem.second_stage.fuel_amount_tons}</p>
+            </>
+          }
 
           <p><span>Success rate: </span>{activeItem.success_rate_pct}%</p>
           <Row>
