2b76cd980e4f291fb3c0e294fd1894e124ffcc13
mbrad26 <mbrad26@gmail.com>
2020-08-03 20:59:11 +0100

    added test for handleRefresh in NavBar component

--- src/tests/NavBar/NavBar.test.js
+++ src/tests/NavBar/NavBar.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Navbar from '../../components/NavBar/NavBar.js';
 import { Context } from '../../components/App/App.js';
 
@@ -15,5 +15,24 @@ describe('NavBar', () => {
     );
 
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('calls handleRefresh', () => {
+    const { location } = window;
+    delete window.location;
+    window.location = { reload: jest.fn() };
+
+    const context = {
+      handleClick: jest.fn()
+    }
+    const { container } = render(
+      <Context.Provider value={context}>
+        <Navbar />
+      </Context.Provider>
+    );
+
+    fireEvent.click(screen.getAllByRole('button')[0]);
+
+    expect(window.location.reload).toHaveBeenCalledTimes(1);
   });
 });
